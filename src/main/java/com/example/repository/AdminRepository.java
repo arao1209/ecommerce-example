da8bbf8bf526ccbac4d91fa04e720a2cf96eb0fd
arao1209 <aneri.ami12@gmail.com>
2022-11-14 20:32:13 -0700

    working repository

---
+++ src/main/java/com/example/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
